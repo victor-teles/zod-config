e879d4e57f32474cb25854c28e5d849c0c69e0ac
Victor <78874691+victor-teles@users.noreply.github.com>
2024-04-20 15:41:37 -0300

    fix: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,6 +7,8 @@ export default defineConfig([
       "json-adapter": "src/lib/adapters/json-adapter/index.ts",
       "env-adapter": "src/lib/adapters/env-adapter/index.ts",
       "dotenv-adapter": "src/lib/adapters/dotenv-adapter/index.ts",
+      "yaml-adapter": "src/lib/adapters/yaml-adapter/index.ts",
+      "localStorage-adapter": "src/lib/adapters/localStorage-adapter/index.ts",
     },
     format: ["cjs", "esm"],
     target: "node14",
